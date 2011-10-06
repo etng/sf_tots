efa155e539b313cb717104fae5bf9196c6c8116c
Yi Bo <etng2004@gmail.com>
2011-10-06 17:55:33 +0800

    just generated

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    tots
+ * @subpackage form
+ * @author     Yi Bo<etng2004@gmail.com> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
